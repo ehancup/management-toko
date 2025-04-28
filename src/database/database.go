2e20ca4b96f32a4afa9cbd94dac2f51ce7c5fe86
ehancup <mondokskuy19@gmail.com>
2025-04-28 23:20:22 +0700

    test 7

--- src/database/database.go
+++ src/database/database.go
@@ -2,25 +2,54 @@ package database
 
 import (
 	// "gin-gorm/app/models"
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/base64"
 	"gin-boilerplate/config"
 	"gin-boilerplate/src/database/dao"
+	"os"
 
 	// "gin-boilerplate/src/app/users"
 	"gin-boilerplate/src/utils/logger"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+
+	mysqlDriver "github.com/go-sql-driver/mysql"
 )
 
 var DB *gorm.DB
 
 func ConnectDatabase() {
 
+	caCert := os.Getenv("CA_CERT")
+	if caCert == "" {
+        logger.Fatal("CA_CERT_BASE64 environment variable not set")
+    }
+
+    caCertBytes, err := base64.StdEncoding.DecodeString(caCert)
+    if err != nil {
+        logger.Fatal("Failed to decode CA certificate from base64: %v","data", err)
+    }
+
+    rootCertPool := x509.NewCertPool()
+    if ok := rootCertPool.AppendCertsFromPEM(caCertBytes); !ok {
+        logger.Fatal("Failed to append decoded CA certificate")
+    }
+
+    tlsConfig := &tls.Config{
+        RootCAs: rootCertPool,
+    }
+
+    err = mysqlDriver.RegisterTLSConfig("custom", tlsConfig)
+	if err != nil {
+        logger.Fatal("Failed to register custom TLS config: %v","data", err)
+    }
 	var errConnection error
 	dbCfg := config.GetConfig().DB
 	appCfg := config.GetConfig().App
 
-	DB, errConnection = gorm.Open(mysql.Open(dbCfg.DSN), &gorm.Config{})
+	DB, errConnection = gorm.Open(mysql.Open(dbCfg.DSN + "&tls=custom"), &gorm.Config{})
 	if (appCfg.Mode == "debug") {DB.Debug()}
 
 	if errConnection != nil {
