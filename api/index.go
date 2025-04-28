7fa361cd7de43c2318b24df2401fa6f381a28736
ehancup <mondokskuy19@gmail.com>
2025-04-28 22:32:15 +0700

    test 4

--- api/index.go
+++ api/index.go
@@ -2,13 +2,13 @@ package handler
 
 import (
 	// "fmt"
-	"flag"
+	// "flag"
 	"gin-boilerplate/config"
 	"gin-boilerplate/src"
 	"gin-boilerplate/src/database"
 	"gin-boilerplate/src/utils/logger"
 	"net/http"
-	"strings"
+	// "strings"
 
 	// "time"
 
@@ -18,7 +18,7 @@ import (
 
 	_ "gin-boilerplate/docs"
 
-	"github.com/AlecAivazis/survey/v2"
+	// "github.com/AlecAivazis/survey/v2"
 	"github.com/swaggo/files"
 	"github.com/swaggo/gin-swagger"
 )
@@ -39,41 +39,41 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 
 	database.ConnectDatabase()
 
-	isMigrate := flag.Bool("migrate", false, "set to migrate database.")
-	flag.Parse()
+	// isMigrate := flag.Bool("migrate", false, "set to migrate database.")
+	// flag.Parse()
 
-	if *isMigrate {
-		p := ""
-		descriptions := map[string]string{
-			"Normal migrate":  "just doing a normal migration (no data will lose)",
-			"Reset migrate": "recommend for delete or edit column name (you will lose all data)",
-		}
-		prompt := &survey.Select{
-			Message: "Select migration type",
-			Options: []string{"Normal migrate", "Reset migrate"},
-			Description: func(value string, index int) string {
-				if desc, ok := descriptions[value]; ok {
-					return desc
-				}
-				return ""
-			},
-		}
-		survey.AskOne(prompt, &p)
-		if strings.HasPrefix(p, "Reset") {
-			sure := false
-			pr := &survey.Confirm{
-				Message: "Are you sure?",
-			}
-			survey.AskOne(pr, &sure)
-			if sure {
-				database.Migrate(true)
-				return
-			} else {return}
-		} else {
-			database.Migrate(false)
-			return
-		}
-	} 
+	// if *isMigrate {
+	// 	p := ""
+	// 	descriptions := map[string]string{
+	// 		"Normal migrate":  "just doing a normal migration (no data will lose)",
+	// 		"Reset migrate": "recommend for delete or edit column name (you will lose all data)",
+	// 	}
+	// 	prompt := &survey.Select{
+	// 		Message: "Select migration type",
+	// 		Options: []string{"Normal migrate", "Reset migrate"},
+	// 		Description: func(value string, index int) string {
+	// 			if desc, ok := descriptions[value]; ok {
+	// 				return desc
+	// 			}
+	// 			return ""
+	// 		},
+	// 	}
+	// 	survey.AskOne(prompt, &p)
+	// 	if strings.HasPrefix(p, "Reset") {
+	// 		sure := false
+	// 		pr := &survey.Confirm{
+	// 			Message: "Are you sure?",
+	// 		}
+	// 		survey.AskOne(pr, &sure)
+	// 		if sure {
+	// 			database.Migrate(true)
+	// 			return
+	// 		} else {return}
+	// 	} else {
+	// 		database.Migrate(false)
+	// 		return
+	// 	}
+	// } 
 
 	
 	appCfg := config.GetConfig().App
